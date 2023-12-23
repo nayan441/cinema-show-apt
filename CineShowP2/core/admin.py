e75880d879e9ec4db627b90ee5960d7fdf8c037a
Nayan Yadav <yadavnayan441@gmail.com>
2023-12-23 17:49:13 +0530

    Added readme.md

--- CineShowP2/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Movie,CinemaHall, MovieTiming, Timings, Seat
-
-admin.site.register(Movie)
-admin.site.register(CinemaHall)
-admin.site.register(MovieTiming)
-admin.site.register(Timings)
-admin.site.register(Seat)
